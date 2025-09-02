8d68ec3f106d3aedb458f4ceb491b91fef22b807
Abban <abban.varughese@gmail.com>
2025-09-02 06:02:36 +0000

    paddle game

---
+++ paddle/Ball.lua
@@ -0,0 +1,43 @@
+require 'class'
+Ball=class{}
+
+function Ball:init(x,y,WIDTH,HEIGHT)
+    self.x=x
+    self.y=y
+    self.WIDTH=WIDTH
+    self.HEIGHT=HEIGHT
+    self.dx=math.random(2)==1 and 100 or -100
+    self.dy=math.random(-50,50)
+end
+
+function Ball:update(dt)
+    self.x=self.x+self.dx*dt
+    self.y=self.y+self.dy*dt
+    if self.y>VIRTUAL_HEIGHT or self.y<0 then
+        self.dy=-self.dy
+    end
+    if self.x<0 then
+        player2.score=player2.score+1
+        game_state='player1Serve'
+        self:reset()
+    elseif self.x>VIRTUAL_WIDTH then
+        player1.score=player1.score+1
+        game_state='player2Serve'
+        self:reset()
+    end
+end
+
+function Ball:reset()
+    self.x=VIRTUAL_WIDTH/2-2
+    self.y=VIRTUAL_HEIGHT/2-2
+    if game_state=='player1Serve' then
+        self.dx=100
+    elseif game_state=='player2Serve' then
+        self.dx=-100
+    end
+    self.dy=math.random(-50,50)
+end
+
+function Ball:draw()
+    love.graphics.rectangle('fill',self.x,self.y,self.WIDTH,self.HEIGHT)
+end
---
+++ paddle/main.lua
@@ -0,0 +1,83 @@
+WINDOW_WIDTH=1280
+WINDOW_HEIGHT=720
+VIRTUAL_WIDTH=432
+VIRTUAL_HEIGHT=243
+
+PADDLE_SIZEY=30
+PADDLE_SPEED=200
+
+push=require 'push'
+class=require 'class'
+require 'Ball'
+require 'Paddle'
+
+function love.load()
+    math.randomseed(os.time())
+    love.graphics.setDefaultFilter('nearest','nearest')
+    font=love.graphics.newFont('font.ttf',24)
+    player1=Paddle(5,20,5,30)
+    player2=Paddle(VIRTUAL_WIDTH-10,VIRTUAL_HEIGHT-20,5,30)
+    love.window.setMode(WINDOW_WIDTH,WINDOW_HEIGHT,{vsync=true,resizable=false,fullscreen=false})
+    push:setupScreen(VIRTUAL_WIDTH,VIRTUAL_HEIGHT,WINDOW_WIDTH,WINDOW_HEIGHT,{upscale='normal'})
+    -- will randomly generate x or -x velocity
+    ball=Ball(VIRTUAL_WIDTH/2-2,VIRTUAL_HEIGHT/2-2,4,4)
+    game_state='start'
+end
+
+function love.update(dt)
+    if game_state=='play' then
+        ball:update(dt)
+        if love.keyboard.isDown('w') then
+            player1.speed=-200
+        elseif love.keyboard.isDown('s') then
+            player1.speed=200
+        else
+            player1.speed=0
+        end
+        
+        if love.keyboard.isDown('up') then
+            player2.speed=-200
+        elseif love.keyboard.isDown('down') then
+            player2.speed=200
+        else 
+            player2.speed=0
+        end
+        player1:update(dt)
+        player2:update(dt)
+    end
+end
+
+function love.keypressed(key)
+    if key=='escape' then
+        love.event.quit()
+    elseif key=='space' then
+        if game_state=='start' then
+            ball:reset()
+            game_state='play'
+        elseif game_state=='player1Serve' or game_state=='player2Serve' then
+            ball:reset()
+            game_state='play'
+        end
+    end
+end
+
+
+function love.draw()
+    push:start()
+    love.graphics.setFont(font)
+    love.graphics.clear(40/255,45/255,52/255,1)
+    if game_state=='start' then
+        love.graphics.printf("Press SPACE to start the game",0,VIRTUAL_HEIGHT-60,VIRTUAL_WIDTH,'center')
+    elseif game_state=='player1Serve' or game_state=='player2Serve' then
+        love.graphics.printf("Press SPACE to serve to the other player.",0,VIRTUAL_HEIGHT-60,VIRTUAL_WIDTH,'center')
+    end
+    -- paddle 1
+    player1:draw()
+    -- paddle 2
+    player2:draw()
+    --BALL
+    ball:draw()
+    love.graphics.print(tostring(player1.score),VIRTUAL_WIDTH/2-70,VIRTUAL_HEIGHT/2-80)
+    love.graphics.print(tostring(player2.score),VIRTUAL_WIDTH/2+50,VIRTUAL_HEIGHT/2-80)
+    push:finish()
+end
